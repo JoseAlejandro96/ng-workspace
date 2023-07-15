7885dedcfb63428c8cb3d7faf25ed7c6ada3ed9f
Jose Alejandro <jscuesta96@gmail.com>
2023-07-15 10:05:37 +0200

    initial select structure

---
+++ projects/custom-form-controls/src/lib/select/select.module.ts
@@ -0,0 +1,11 @@
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  imports: [CommonModule, OverlayModule]
+})
+export class SelectModule { }
